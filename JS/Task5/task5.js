32c96d8d50c4a0541b2c4e6ab89c954fcbecc7a0
KristinaIvanova <kristina.ivd@gmail.com>
2016-06-17 18:30:43 +0300

    task 5 added

--- JS/Task5/task5.js
+++ JS/Task5/task5.js
@@ -6,6 +6,19 @@ var numbers = new Array(20);
         console.log("array[" + i + "] = " + numbers[i]);
     }
  //---------------------------------------------------------
+ //Problem 2. Lexicographically comparison
+ var array1 = ["x", "y", "z", "a", "b", "c"];
+ var array2 = ["a", "b", "c", "d", "e", "f"];
+ var string1 = array1.join(",");
+ var string2 = array2.join(",");
+ if (string1 > string2) {
+     console.log("string1 > string2");
+ } else if (string1 < string2) {
+     console.log("string1 < string2");
+ } else if (string1 === string2) {
+     console.log("string1 = string2");
+ }  
+ //---------------------------------------------------------
  //Problem 3. Maximal sequence
  function findMaxSeq(num) {
     var currentSequence = 1,
@@ -26,7 +39,7 @@ var numbers = new Array(20);
     }
     // create arr with the results
     var result = [];
-    for (var i = 0; i < longestSequence; i++) {
+    for (var i = 0; i < longestSequence; i+=1) {
         result.push(maxSeqNum);
     }
     // result
@@ -42,7 +55,7 @@ function findMaxIncrSeq(num) {
         indexOfLongest;
 
     for (var i = 0; i < num.length; i+=1) {
-        if (num[i] < num[i + 1]) {
+        if (num[i] < num[i + 1] && num[i]+1 === num [i + 1] ) {
             currentSequence++;
         } else {
             if (longestSequence < currentSequence) {
@@ -64,5 +77,79 @@ function findMaxIncrSeq(num) {
 }
 
 findMaxIncrSeq([1, 2, 3, 4, 2, 2, 4]);
-findMaxIncrSeq([3, 2, 3, 4, 1, 2, 2, 4]);
+findMaxIncrSeq([3, 2, 3, 4, 9, 1, 2, 2, 4]);
 findMaxIncrSeq([3, 2, 1]);
+//---------------------------------------------------------
+//Problem 5. Selection sort
+//Sort from a[0] to a[n-1]
+var arr = [6, 3, 5, 6 ,7];
+var sortedArr = [];
+
+ for (var i = 0; i < arr.length; i+=1) {
+            var index = i;
+            for (var j = i + 1; j < arr.length; j+=1) {
+                if (arr[index] > arr[j])
+                    index = j;
+
+                var smallerNumber = arr[index];
+                arr[index] = arr[i];
+                arr[i] = smallerNumber;
+            }
+             sortedArr.push(arr[i]);
+        }
+            console.log(sortedArr);
+//---------------------------------------------------------
+//Problem 6. Most frequent number
+var arr = [5, 7, 9, 3, 7, 1, 9, 5, 7, 9, 7, 7];
+
+var current = 0;
+        var counter = 0;
+        var savedCurrent = 0;
+        var SavedCounter = 0;
+
+        for (var i = 0; i < arr.length; i+=1) {
+            current = arr[i];
+            counter = 0;
+
+            for (var j = 0; j < arr.length; j+=1) {
+                if (current === arr[j])
+                {
+                    counter+=1;
+                }
+                if ((j === arr.length - 1) && (counter >= SavedCounter)) {
+                    savedCurrent = current;
+                    SavedCounter = counter;
+                }
+            }
+        }
+
+        if (counter > SavedCounter) {
+            console.log('Number: '+ current+' - '+counter+' times');
+        } else {
+            console.log('Number: ' + savedCurrent+' - '+SavedCounter+' times');
+        }
+//-----------------------------------------------------------
+//Problem 7. Binary search
+
+var array = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22];
+        var searchedNumber = 7;
+        var startSearch = 0;
+        var endSearch = array.length - 1;
+        var middle;
+        while (startSearch <= endSearch)
+        {
+            middle = parseInt((startSearch + endSearch) / 2);
+            if (array[middle] === searchedNumber)
+            {
+                console.log("Index is: " + middle);
+                break;
+            }
+            else if (array[middle] < searchedNumber)
+            {
+                startSearch = middle + 1;
+            }
+            else if (array[middle] > searchedNumber)
+            {
+                endSearch = middle - 1;
+            }
+        }
